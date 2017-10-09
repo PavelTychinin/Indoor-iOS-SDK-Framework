6eac85d99ff31387f092e4d3da657f7a476de161
Pavel <p.tychinin@navigine.com>
2017-10-09 12:29:44 +0300

    Add NCCategory

---
+++ Navigine Framework/Navigine.framework/Headers/NCCategory.h
@@ -0,0 +1,14 @@
+//
+//  NCCategory.h
+//  NavigineSDK
+//
+//  Created by Pavel Tychinin on 03/10/2017.
+//  Copyright © 2017 Navigine. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NCCategory : NSObject
+@property (nonatomic, assign) NSInteger id;
+@property (nonatomic, strong) NSString *name;
+@end
